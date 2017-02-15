204e246ed1eee7369e22c3e1efa28cc2cb529509
Dave Langer <david_langer@hotmail.com>
2017-02-15 22:42:07 +0100

    Code Update
    Code update for Feb 2017 Amsterdam Bootcamp.

--- Text Analytics/TF-IDF.R
+++ Text Analytics/TF-IDF.R
@@ -1,8 +1,9 @@
 ###################################################################################
 ## This code is part of Data Science Dojo's bootcamp
-## Copyright (C) 2015~2016
+## Copyright (C) 2015~2017
 
-## Objective: Building TF-IDF matrices using R, including stemming for dictionary compression
+## Objective: Perform Text Analysis preprocessing tasks and evaluate document
+##            similarity via TF-IDF.
 ## Packages: tm, lsa
 ###################################################################################
 
@@ -10,21 +11,51 @@
 library(tm)
 library(lsa)
 
+
 # Load the test dataset
 data(crude)
 
-# Explore the dataset
-summary(crude)
 
-# Remove punctuation, apply a stemmer, and build a document-term matrix using TF-IDF
-crude.dt <- DocumentTermMatrix(crude, control=list(weighting=weightTfIdf,
-                                                   removePunctuation=T,
-                                                   stemming=T))
+# Look at the help file of the dataset
+?crude
 
-# Inspect the document-term matrix
-print(crude.dt)
-#inspect(crude.dt)
 
-#Compute a matrix of cosine similarity scores between each document pair
+# Display the raw text of the first document in the corpus and display.
+writeLines(as.character(crude[[1]]))
+
+
+# Transform document words to lower case
+crude <- tm_map(crude, content_transformer(tolower))
+writeLines(as.character(crude[[1]]))
+
+
+# Remove punctuation from documents
+crude <- tm_map(crude, removePunctuation)
+writeLines(as.character(crude[[1]]))
+
+
+# Remove stopwords from the corpus
+crude <- tm_map(crude, removeWords, stopwords("english"))
+writeLines(as.character(crude[[1]]))
+
+
+# Remove numbers from the corpus
+crude <- tm_map(crude, removeNumbers)
+writeLines(as.character(crude[[1]]))
+
+
+# Stem the corpus
+crude <- tm_map(crude, stemDocument, language = "english")
+writeLines(as.character(crude[[1]]))
+
+
+# Build a document-term matrix using TF-IDF and inspect
+crude.dt <- DocumentTermMatrix(crude, control=list(weighting=weightTfIdf))
+crude.dt
+inspect(crude.dt[1:8, 1:8])
+
+
+# Compute a matrix of cosine similarity scores between each document pair 
+# and inspect
 crude.cos <- cosine(as.matrix(t(crude.dt)))
-print(crude.cos)
+crude.cos[1:8, 1:8]
